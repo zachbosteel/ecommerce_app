bbf678647dab6ab99d1dfabaf4bb6e4b77a1d253
Zach Bosteel <zbosteel@gmail.com>
2015-06-14 10:15:00 -0500

    Heroku database changes.

--- db/migrate/20150605004654_change_description_and_price_and_add_in_stockto_products.rb
+++ db/migrate/20150605004654_change_description_and_price_and_add_in_stockto_products.rb
@@ -1,7 +1,7 @@
 class ChangeDescriptionAndPriceAndAddInStocktoProducts < ActiveRecord::Migration
   def change
     change_column :products, :description, :text
-    change_column :products, :price, :decimal, precision: 12, scale: 2
+    change_column :products, :price, :decimal, price: :decimal, precision: 12, scale: 2
     add_column :products, :in_stock, :boolean, :default => true
   end
 end
